924a0259e959172fcd8b7331a1fbb9612d9ab8c2
Vinay <vinay.r@unibrain.in>
2024-11-13 12:52:36 +0530

    Updates

---
+++ precache-manifest.130d6efa7b90fe289a8e4e811e6c6fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8274f18c7c24661c21e94c7d23322bb7",
+    "url": "/keeperapp/index.html"
+  },
+  {
+    "revision": "1199841ae5f6226fdccb",
+    "url": "/keeperapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/keeperapp/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/keeperapp/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "1199841ae5f6226fdccb",
+    "url": "/keeperapp/static/js/main.7303d6ce.chunk.js"
+  },
+  {
+    "revision": "df4b535a00e4f974676a",
+    "url": "/keeperapp/static/js/runtime~main.b00994d4.js"
+  }
+]);
